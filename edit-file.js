347e970ae9b1f79ae17c6da84b5e095edc322080
abferris <abferris@gmail.com>
2019-03-20 21:45:08 -0700

    tried so many things to make the promise work. It didnt. will do readme then submit

--- edit-file.js
+++ edit-file.js
@@ -2,18 +2,29 @@
 //requires
 const fs = require('fs');
 const faker = require('faker')
-const file = `${__dirname}/files/test.txt`;
+// const file = `${__dirname}/files/test.txt`;
 
-fs.readFile(file, (err,data) => {
+/** Reads file, and gives back data that was there in console.
+ * param file, error and data
+ */
+fs.readFile(extension, (err,data) => {
+  const file = `${__dirname}/${extension}`;
+
   if (err){ 
     console.error(err);
     return; }
     console.log(data);
   let contents =(faker.random.number()).toString();
+/** Interior function which takes replaces contents with a random number as a string
+ * param file, contents, err, data
+ */
   fs.writeFile(file, Buffer.from(contents), (err,data) =>{
     if (err) {
       console.error(err); 
       return;}
+/** Last interior function which reads the new file data and returns it
+ * param, err data
+ */
       fs.readFile(file, (err,data)=>{
         if (err) {
           console.error(err);
@@ -22,5 +33,4 @@ fs.readFile(file, (err,data) => {
         console.log(data.toString)
       });
   });
-
 });
